8019ec7970cffc2bb7465fd56ff80792b0081305
karan <karan@quokkalabs.com>
2024-02-21 23:33:02 +0530

    Input label list working

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 let Title_Val = document.getElementById('title_value')
 let SelectorType = document.getElementById('filedselector')
 let OutputSection = document.getElementById('AllData')
+let UL_parent= document.createElement('ol')
 
 
 
@@ -9,24 +10,30 @@ let Sub = document.getElementById('AddBtn')
 function Submit(){
 
     if(Title_Val.value !== ''){
+
         let Label = document.createElement('label')
         Label.classList.add('testLabel')
         Label.textContent=Title_Val.value
-        
-        console.log(OutputSection);
-        OutputSection.append(Label)
-    // alert(Title_Val.value)
 
         
+        let Ul_child = document.createElement('li')
+        Ul_child.append(Label)
+        UL_parent.append(Ul_child)
     }
+
+
     else{
         alert('enter value')
     }   
 
+            
+    OutputSection.append(UL_parent)
+
     Title_Val.value = ''
-    // alert(Title_Val.value)
     Title_Val.setAttribute('autofocus','')
     
-}
+};
+
+
 
 // OutputSection
